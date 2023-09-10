1ee6de8d5f83e4a3776d42e1a432f1606585ded6
Johnsonolu <johnsonoluwasegun19@gmail.com>
2023-09-10 22:16:59 +0200

    All tasks completed

---
+++ file.js
@@ -0,0 +1,16 @@
+const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDate = new Date();
+  const currentDayOfWeek = daysOfWeek[currentDate.getDay()];
+  const currentUTCTimeMilliseconds = Date.now();
+  
+  document.getElementById("currentDayOfTheWeek").textContent = currentDayOfWeek;
+  document.getElementById("currentUTCTime").textContent =
+    currentUTCTimeMilliseconds;
